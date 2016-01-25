b090f3baf139bfdbe5c75ed9fb3b6f9c14e8690e
Joachim Pileborg <joachimp@axis.com>
2016-01-25 10:10:26 +0100

    Added area files

---
+++ src/area.c
@@ -0,0 +1,5 @@
+//
+// Created by joachimp on 2016-01-25.
+//
+
+#include "area.h"
---
+++ src/area.h
@@ -0,0 +1,9 @@
+//
+// Created by joachimp on 2016-01-25.
+//
+
+#ifndef PTMUD_NEXT_AREA_H
+#define PTMUD_NEXT_AREA_H
+
+
+#endif //PTMUD_NEXT_AREA_H
